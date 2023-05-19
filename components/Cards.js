b05856f9a0e43abd39442346bc3a452b279abf43
Cervantes Jhon <jhonfredycercha@hotmail.com>
2023-05-18 22:33:03 -0500

    feat(*): agregando paginacion

--- components/Cards.js
+++ components/Cards.js
@@ -5,9 +5,10 @@ const Cards = ({ nasaData }) => {
     <div className="cards">
       {nasaData.map((item) => (
         <Card
-          image={item.links[0].href}
-          title={item.data[0].title}
-          description={item.data[0].description_508}
+          key={item?.data[0]?.nasa_id}
+          image={(item?.links)?item?.links[0]?.href: "https://via.placeholder.com/300"}
+          title={item?.data[0]?.title}
+          description={item?.data[0]?.description_508}
         />
       ))}
     </div>
